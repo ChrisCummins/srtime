f5c7f5ecf4f64967252f661f132f4de96ffd53a1
Chris Cummins <chrisc.101@gmail.com>
2014-11-16 20:25:44 +0000

    Spawn processes using pexpect library
    This allows for much greater programmatic control of the process than
    simply using the os.system() call, which is limited in its capabilities.

--- srtime/process.py
+++ srtime/process.py
@@ -1,33 +1,26 @@
 from datetime import datetime
 import logging as log
-import os
+import pexpect
+
 
 class Process:
-    @staticmethod
-    def _get_tmpfile():
-        epoch = datetime.utcfromtimestamp(0)
-        delta = datetime.now() - epoch
-        timestamp = round(delta.total_seconds() * 1000)
-        return "/tmp/srtime-{0}".format(timestamp)
-
     def __init__(self, options):
-        cmd = options.command
-        tmpfile = self._get_tmpfile()
-        cmd += " 2>&1 | tee {0}".format(tmpfile)
-
-        log.info("Command: {0}".format(cmd))
-
         # Flush the system caches prior to executing the command:
         if options.flush_caches:
             flush_system_caches()
 
-        # Execute the command:
+        # Spawn the process:
         start = datetime.now()
-        os.system(cmd)
+        process = pexpect.spawn(options.args, timeout=None)
+
+        # Buffer the output:
+        for line in process:
+            print(line.decode('raw_unicode_escape').rstrip())
+
+        # Wait until the process terminates:
+        process.close()
         end = datetime.now()
 
-        # Remove tempfile:
-        os.system("rm -f {0}".format(tmpfile))
 
         # Return elapsed time:
         self._time = end - start
